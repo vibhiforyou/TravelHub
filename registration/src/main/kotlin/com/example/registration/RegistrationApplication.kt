bac3da1c9484e18947588c36a7c2408e5f1807c7
vibhiforyou <vaibhav.a.dhamale@gmail.com>
2023-06-12 22:02:45 +0200

    multi module project structure creation

---
+++ registration/src/main/kotlin/com/example/registration/RegistrationApplication.kt
@@ -0,0 +1,11 @@
+package com.example.registration
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RegistrationApplication
+
+fun main(args: Array<String>) {
+	runApplication<RegistrationApplication>(*args)
+}
